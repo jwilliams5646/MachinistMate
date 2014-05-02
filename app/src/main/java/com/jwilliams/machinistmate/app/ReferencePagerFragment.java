dc7aa9df0cb623162f7551f199118fd72dc33406
John Williams <john.williams@systemware.com>
2014-05-02 10:11:02 -0500

    Modified pager background, changed tab indicator.

--- app/src/main/java/com/jwilliams/machinistmate/app/ReferencePagerFragment.java
+++ app/src/main/java/com/jwilliams/machinistmate/app/ReferencePagerFragment.java
@@ -1,8 +1,10 @@
 package com.jwilliams.machinistmate.app;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,12 +18,16 @@ public class ReferencePagerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        View result = inflater.inflate(R.layout.reference_pager, container, false);
-        ViewPager pager = (ViewPager) result.findViewById(R.id.pager);
+        View rootView = inflater.inflate(R.layout.reference_pager, container, false);
+        ViewPager pager = (ViewPager) rootView.findViewById(R.id.pager);
+
+        PagerTabStrip pagerTabStrip = (PagerTabStrip) rootView.findViewById(R.id.pager_tab_strip);
+        pagerTabStrip.setDrawFullUnderline(true);
+        pagerTabStrip.setTabIndicatorColor(Color.parseColor("#0099CC"));
 
         pager.setAdapter(buildAdapter());
 
-        return (result);
+        return rootView;
     }
     private PagerAdapter buildAdapter() {
         return(new ReferenceAdapter(getActivity(), getChildFragmentManager()));
