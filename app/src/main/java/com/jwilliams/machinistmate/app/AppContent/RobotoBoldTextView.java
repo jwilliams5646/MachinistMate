fa8f6aa050f825c71738e75f5751a87ee7a15fa5
John Williams <john.williams@systemware.com>
2014-05-05 16:53:33 -0500

    Added some fonts

---
+++ app/src/main/java/com/jwilliams/machinistmate/app/AppContent/RobotoBoldTextView.java
@@ -0,0 +1,38 @@
+package com.jwilliams.machinistmate.app.AppContent;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by john.williams on 5/5/2014.
+ */
+public class RobotoBoldTextView  extends TextView {
+    public RobotoBoldTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public RobotoBoldTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RobotoBoldTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            try {
+                Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Roboto-Black.ttf");
+                setTypeface(tf);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
